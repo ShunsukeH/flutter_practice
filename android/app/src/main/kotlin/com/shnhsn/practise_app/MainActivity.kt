a9ecb4203c200caf32446aa4ec7b29d0181e3ef2
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2019-12-30 09:53:22 +0900

    Initial Commit

---
+++ android/app/src/main/kotlin/com/shnhsn/practise_app/MainActivity.kt
@@ -0,0 +1,14 @@
+@file:Suppress("Annotator", "Annotator")
+
+package com.shnhsn.practise_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
